128b07411b0bf35d5e06c043691e99f40e59bfdc
maelic <teoneau@gmail.com>
2021-03-24 19:25:03 +0100

    update

--- src/robobreizh_perception/dependencies/install/maskrcnn/install.sh
+++
@@ -1,4 +0,0 @@
-# From: https://github.com/facebookresearch/detectron2/blob/master/INSTALL.md
-cd ~/
-python3.7 -m pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
-
--- src/robobreizh_perception/dependencies/install/python_dependencies/install.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install python3-pip
-sudo python3.7 -m pip install -r requirements.txt

1022dd53004a31fd1e4acabe4bd5987d878593f9
celebo <cedriclebono@protonmail.com>
2021-03-25 20:47:15 +0100

    Perception: Adding video demo.

--- src/robobreizh_perception/src/detection/maskrcnn/maskrcnn.py
+++ src/robobreizh_perception/src/detection/maskrcnn/maskrcnn.py
@@ -1,12 +1,14 @@
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.utils.visualizer import ColorMode
 from detectron2.data import MetadataCatalog
 
 import numpy as np
 import cv2
 import time
+
+from detection.maskrcnn.hacked_visualizer import HackedVisualizer
 
 cfg = get_cfg()
 # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
@@ -24,7 +26,7 @@ def mask_rcnn(im):
     Perform the mask RCNN detection
     '''
     outputs = predictor(im)
-    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2, instance_mode=ColorMode.SEGMENTATION)
+    v = HackedVisualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2, instance_mode=ColorMode.SEGMENTATION)
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     image = out.get_image()
     if SAVE_IMG:
